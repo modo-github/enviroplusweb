c338a09d1d96f81ab2efc2f71e6962e9eb47fe6e
idotj <info@idotj.com>
2022-03-17 22:29:25 +0100

    JS

--- static/js/main.js
+++ static/js/main.js
@@ -63,7 +63,7 @@ function getData() {
 
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            console.log('responseText: ', this.responseText.json());
+            console.log('responseText: ', JSON.parse(this.responseText));
             particulate_sensor_readings = true;
             gas_sensor_readings = true;
         }
