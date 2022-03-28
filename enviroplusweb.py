e4953ed64c7f9ccad1878e68b4d81fb00a3ebccf
idotj <info@idotj.com>
2022-03-28 20:01:57 +0200

    ppm

--- enviroplusweb.py
+++ enviroplusweb.py
@@ -163,19 +163,21 @@ def read_data(time):
 
     if gas_sensor:
         gases = gas.read_all()
-        # Pass ohms to ppm
-        # oxi_base = 20000
-        # oxi = round(gases.oxidising, 0)
-        oxi = round(gases.oxidising / 1000, 1)
-        # red_base = 200000
-        # red = round(gases.reducing, 0)
-        red = round(gases.reducing / 1000)
-        # nh3 = round(gases.nh3, 0)
-        # nh3_base = 750000
-        nh3 = round(gases.nh3 / 1000)
-        # red_in_ppm = math.pow(10, -1.25 * math.log10(red/red_base) + 0.64)
-        # oxi_in_ppm = math.pow(10, math.log10(oxi/oxi_base) - 0.8129)
-        # nh3_in_ppm = math.pow(10, -1.8 * math.log10(nh3/nh3_base) - 0.163)
+
+        oxi_base = 20000
+        oxi_raw = round(gases.oxidising, 0)
+        oxi = math.pow(10, math.log10(oxi_raw/oxi_base) - 0.8129)
+        # oxi = round(gases.oxidising / 1000, 1)
+
+        red_base = 200000
+        red_raw = round(gases.reducing, 0)
+        red = math.pow(10, -1.25 * math.log10(red_raw/red_base) + 0.64)
+        # red = round(gases.reducing / 1000)
+
+        nh3_raw = round(gases.nh3, 0)
+        nh3_base = 750000
+        nh3 = math.pow(10, -1.8 * math.log10(nh3_raw/nh3_base) - 0.163)
+        # nh3 = round(gases.nh3 / 1000)
     else:
         oxi = red = nh3 = 0
 
